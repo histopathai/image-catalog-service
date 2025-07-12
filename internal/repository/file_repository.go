90ac7f68d4111221b784d71d3c17ad23b9dd21a9
yasintuncerr <yasintuncerr@gmail.com>
2025-07-12 13:50:34 +0300

    Update Read method in FileRepository to return io.ReadCloser instead of multipart.File

--- internal/repository/file_repository.go
+++ internal/repository/file_repository.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"context"
+	"io"
 	"mime/multipart"
 )
 
 type FileRepository interface {
 	Create(ctx context.Context, file multipart.File, filepath string) error
-	Read(ctx context.Context, filepath string) (multipart.File, error)
+	Read(ctx context.Context, filepath string) (io.ReadCloser, error)
 	Update(ctx context.Context, file multipart.File, filepath string) error
 	Delete(ctx context.Context, filepath string) error
 }
