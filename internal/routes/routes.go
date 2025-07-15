962939104d72436b552bbd9149c5760f9c138649
yasintuncerr <yasintuncerr@gmail.com>
2025-07-15 13:00:30 +0300

    Remove UploadImage route from API

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,7 +10,6 @@ func SetupRouter(imageHandler *handlers.ImageHandler) *gin.Engine {
 
 	apiV1 := router.Group("/api/v1")
 	{
-		apiV1.POST("/images", imageHandler.UploadImage)
 		apiV1.GET("/images/:image_id", imageHandler.GetImageByID)
 		apiV1.PUT("/images/:image_id", imageHandler.UpdateImageByID)
 		apiV1.DELETE("/images/:image_id", imageHandler.DeleteImageByID)
